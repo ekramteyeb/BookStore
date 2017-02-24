a49fd46a4ba749f0206d77e91ca576841cfa733d
teyeb <hussenab@gmail.com>
2017-02-24 23:33:41 +0200

    controller edited , index template added

--- BookStore/src/main/java/com/course/bookstore/controllers/BookControllers.java
+++ BookStore/src/main/java/com/course/bookstore/controllers/BookControllers.java
@@ -1,9 +1,14 @@
 package com.course.bookstore.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookControllers {
 
-	
+	@RequestMapping(value="/index")
+	public String index(){
+		
+		return "index";
+	}
 }
