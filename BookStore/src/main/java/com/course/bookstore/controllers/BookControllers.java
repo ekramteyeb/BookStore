fa8253412a2f23740374378f33e77a62badcb41c
teyeb <hussenab@gmail.com>
2017-03-08 08:40:59 +0200

    plugin added

--- BookStore/src/main/java/com/course/bookstore/controllers/BookControllers.java
+++ BookStore/src/main/java/com/course/bookstore/controllers/BookControllers.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookControllers {
 
+	@RequestMapping(value="/")
+	public String home(){
+		
+		return "index";
+	}
 	@RequestMapping(value="/index")
 	public String index(){
 		
